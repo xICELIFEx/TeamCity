869589442efba1bad853f73fd587ef628044fe6b
Gennady Hieber <gennady.hieber@gmail.com>
2016-05-26 23:46:37 +0300

    Init

---
+++ TeamCity/Assets/Scripts/ButtonClickHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonClickHandler : MonoBehaviour
+{
+	[SerializeField]
+	private Text _text;
+
+	public void OnButtonClickHandler()
+	{
+		_text.text = "Hello world";
+	}
+}
